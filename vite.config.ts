161db021974fb6b0d2a20123299052171264734e
David Wursteisen <david.wursteisen+git@gmail.com>
2026-03-16 14:55:59 +0100

    config github

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/setlit/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/GigSheet/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
